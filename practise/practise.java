51bc5a03b8187f7571ffd909bd99101939a3255f
sivaprasad.turaka@gmail.com <sivaprasad.turaka2001@gmail.com>
2024-03-27 09:02:38 +0530

    after reset har

--- practise/practise.java
+++ practise/practise.java
@@ -6,5 +6,6 @@ import java.lang.reflect.Array;
 public class practise{
 	public static void main(String[] args) {  
 		System.out.println("changing text");
+		System.out.println("hello doctor");
 		
 	}}
