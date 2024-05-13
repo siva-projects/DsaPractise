a76a3a2797c20ec64e473c52f6893268969b87e1
sivaprasad.turaka@gmail.com <sivaprasad.turaka2001@gmail.com>
2024-05-13 11:24:27 +0530

    added binarySearchTree implementation

--- Trees/Trees/BinarySearchTree.java
+++ Trees/Trees/BinarySearchTree.java
@@ -1,5 +1,53 @@
-package Trees;
+
 
 public class BinarySearchTree {
+//    public class node{
+//        public int value;
+//        private int height;
+//        private Node left;
+//        private Node right;
+//    }
+//    public void Node(int value){
+//        this.value = value;
+//    }
+//    public int getValue(){
+//        return value;
+//    }
+//
+//    private root Node;
+//
+//    public BinarySearchTree(){}
+//    public int height(Node node){
+//        if(node == null){
+//            return -1;
+//        }
+//        return node.height;
+//    }
+//
+//    public boolean isEmpty(){
+//        return root == null;
+//    }
+//
+//    public void display(){
+//        display(root, "root Node: ");
+//    }
+//
+//    private void display(Node node, String details){
+//        if(node == null){
+//            return;
+//        }
+//        System.out.println(details + node.getValue());
+//        
+//        display(node.left, "left child of "+node.getValue()+" : ");
+//
+//        display(node.right, "right child of "+node.getValue()+" : ");
+//    }
+//    
+    public static void main(String[] args) {
+    	System.out.println("This is working priya");
+
+
+        
+    }
     
 }
